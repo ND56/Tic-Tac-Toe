109372761d0a2205fb23c8fc5b0701f0d1642949
ND56 <ndrew56@gmail.com>
2018-02-06 13:47:43 -0500

    Began working on the "game engine" set of tasks.
    Did not finish yet. Tasks include the following:
      Create Empty Board in JS
      Add to Board
      Turn rotates between x and o
      Can not choose already occupied spots
      Check Board for Winner

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,8 +7,27 @@ $(() => {
   setAPIOrigin(location, config)
 })
 
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
+// game board
+const gameBoard = ['', '', '', '', '', '', '', '', '']
 
-// use require without a reference to ensure a file is bundled
-// require('./example')
+// add to board
+const selectSpace = function (paramForGameBoard) {
+  // when user first selects a space, the space changes to X
+  // if no value is "X" or "Y", first value is X
+  // the values alternate from O to X thereafter
+  let emptySpaces = 0
+  paramForGameBoard.forEach(function (paramForArrayElement) {
+    if (paramForArrayElement = '') {
+      emptySpaces += 1
+    }
+  })
+  if (emptySpaces = //check to see if odd)
+
+
+
+
+
+  // you cannot choose already occupied spots
+
+  // execute checkForWin function
+}
